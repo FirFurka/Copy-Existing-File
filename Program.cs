8b4814c3ec29067a4998fbe1be9c6cd901c2da8a
Furka <50808593+FirFurka@users.noreply.github.com>
2024-07-15 22:36:27 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System.Windows.Forms;
+
+namespace CopyAFile
+{
+    internal class CopyAFile
+    {
+        private static void Main(string[] args)
+        {
+            string sourceFile = Application.ExecutablePath.ToString();
+            File.Copy(sourceFile, @"D:\GitHub\FileCopy\copyTesting.exe");
+        }
+    }
+}
